9dcb1ee8b5853105e875403b77a4e22acf7cbf1b
nidhiaelius <nidhi.aastani@gmail.com>
2025-07-15 08:46:04 +0530

    update

--- test_codes/p03_variables/src/main.rs
+++ test_codes/p03_variables/src/main.rs
@@ -6,5 +6,25 @@ fn main() {
     x = 10;
     println!("New value of x is {x}");
 
+    const a: u32 = 10;
+    println!("Value of constant 'a' is: {}", a);
 
+    let b = 10;
+    let b = b + 20;
+    {
+        let b = b * 10;
+        println!("Value of b in scope: {}", b);
+    }
+    println!("Value of b in outside scope is : {}", b);
+
+    let c = "sachin";
+    let c = c.len();
+    println!("Length of string is {c}");
+    println!("string c is {}", c.to_string());
+    
+    let s = "3.14";
+    let f: f64 = s.parse().unwrap();
+
+    let i: i32 = "100".parse().expect("Not a number");
+    println!("Value of i is {}", i);
 }
